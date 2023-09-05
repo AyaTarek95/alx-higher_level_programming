d52dda2c077d9d175f6a804ccf8d8fe29abf74a0
AyaTarek95 <aya.tarek213@icloud.com>
2023-09-05 16:39:58 +0000

    fix 7,8,10,11,12

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    #function to check wether the character passed is lowercase or othrwise
+#function to check wether the character passed is lowercase or othrwise
     if ord(c) >= 97 and ord(c) <= 122:
         return True
     else:
