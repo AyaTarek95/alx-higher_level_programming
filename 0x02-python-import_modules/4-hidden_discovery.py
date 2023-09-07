bf71bdfd5a104f55f33fb9c6e6df5f9633d9e7f2
AyaTarek95 <aya.tarek213@icloud.com>
2023-09-07 15:39:56 +0000

    0, 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    data = dir(hidden_4)
-    for name in data:
+
+    names = dir(hidden_4)
+    for name in names:
         if name[:2] != "__":
             print(name)
 
