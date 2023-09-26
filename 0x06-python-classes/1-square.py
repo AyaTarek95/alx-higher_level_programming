c2f4d29df15bcdd9c14c2394391e10e6308a6b94
AyaTarek95 <aya.tarek213@icloud.com>
2023-09-26 12:59:17 -0700

    fix task1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 
-'''create a Square class'''
+'''Create a Square class'''
 
 class Square:
     '''Square class definition'''
+
     def __init__(self, size):
         '''initialize square
+
         args: 
-            size of the Square'''
+            size of the Square
+        '''
         self.__size = size
