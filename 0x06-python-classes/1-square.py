1a023bcb96da81fdef5b4aaa1d5adc78adb8a2ec
AyaTarek95 <aya.tarek213@icloud.com>
2023-09-26 12:56:30 -0700

    task1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+'''create a Square class'''
+
+class Square:
+    '''Square class definition'''
+    def __init__(self, size):
+        '''initialize square
+        args: 
+            size of the Square'''
+        self.__size = size
