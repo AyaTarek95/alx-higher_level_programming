f4caf75e677ad7afdce412cbcd23e477f99964ff
AyaTarek95 <aya.tarek213@icloud.com>
2023-10-09 07:51:00 -0700

    task6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module of class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Class BaseGeometry."""
+    def area(self):
+        """Method claculates area."""
+        raise Exception("area() is not implemented")
