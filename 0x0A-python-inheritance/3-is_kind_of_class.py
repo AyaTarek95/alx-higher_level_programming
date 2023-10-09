9e929c0191b258db2c3e2261d4a313bb695b5de2
AyaTarek95 <aya.tarek213@icloud.com>
2023-10-09 07:30:53 -0700

    task3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module for method is_kind_of_class."""
+
+
+def is_kind_of_class(obj, a_class):
+    """check wether if the object is derived from specific class."""
+    return isinstance(obj, a_class)
