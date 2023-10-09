901ccd63ce5708c8f378f605ac2981250afe2469
AyaTarek95 <aya.tarek213@icloud.com>
2023-10-09 08:49:58 -0700

    task10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module of class square."""
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    """Subclass to Rectangle class."""
+    def __init__(self, size):
+        """Initialize new square."""
+        self.__size = size
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+    def area(self):
+        """Calculate area method."""
+        return self.__size **2
