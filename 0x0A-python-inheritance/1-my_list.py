e525bbb9af383076e3d87561065ef85360c60490
AyaTarek95 <aya.tarek213@icloud.com>
2023-10-09 07:06:01 -0700

    task1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Mylist class.
+"""
+
+
+class MyList(list):
+    """Derived from list."""
+    def __init__(self):
+        """Initialize the object."""
+        super().__init__()
+
+    def print_sorted(self):
+        """Print sorted list."""
+        print(sorted(self))
