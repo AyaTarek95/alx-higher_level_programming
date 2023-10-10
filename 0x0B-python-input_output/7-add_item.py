35ba0c05748b1bd7b6eda59164b683b2f52547ab
AyaTarek95 <aya.tarek213@icloud.com>
2023-10-10 10:14:13 -0700

    task7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Add all arguments to a Python list, and then save them to a file."""
+import sys
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+args = list(sys.argv[1:])
+
+try:
+    data = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    data = []
+data.extend(args)
+save_to_json_file(data, "add_item.json")
