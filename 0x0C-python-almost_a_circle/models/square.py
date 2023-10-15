e973a01e44825789e5e2ea7bcd390b093c622dfe
AyaTarek95 <aya.tarek213@icloud.com>
2023-10-15 08:31:25 -0700

    till task12

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,42 @@
+#!/usr/in/python3
+'''Define a class square.'''
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    '''Represent a square.'''
+    def __init__(self, size, x=0, y=0, id=None):
+        '''Initialize a new square.'''
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        '''Set/get size of square.'''
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        '''Str representaion of a square.'''
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
+
+    def my_update(self, id=None, size=None, x=None, y=None):
+        '''Update my attributes.'''
+        if id is not None:
+            self.id = id
+        if size is not None:
+            self.size = size
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
+
+    def update(self, *args, **kwargs):
+        '''Update square.'''
+        if args:
+            self.my_update(*args)
+        elif kwargs:
+            self.my_update(**kwargs)
