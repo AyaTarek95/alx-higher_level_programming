2d4142a73be66a2b7c02e6158b4aa18d4d6004ec
AyaTarek95 <aya.tarek213@icloud.com>
2023-10-17 12:21:13 -0700

    fix5

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
-"""Defines a Rectangle class."""
+'''Defines a Rectangle class.'''
 
 
 class Rectangle:
-    """Represent a Rectangle."""
+    '''Represent a Rectangle.'''
 
     def __init__(self, width=0, height=0):
-        """Initiate a new Rectangle.
+        '''Initiate a new Rectangle.
 
         Args:
             width (int): the width of rectangle.
             height (int): the height of rectangle.
-        """
+        '''
         self.__width = width
         self.__height = height
 
     @property
     def width(self):
-        """Get/set width of rectangle."""
+        '''Get/set width of rectangle.'''
         return self.__width
 
     @width.setter
@@ -30,7 +30,7 @@ class Rectangle:
 
     @property
     def height(self):
-        """Get/set height of Rectangle."""
+        '''Get/set height of Rectangle.'''
         return self.__height
 
     @height.setter
@@ -42,20 +42,17 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        """Return area of Rectangle."""
+        '''Return area of Rectangle.'''
         return self.__width * self.__height
 
     def perimeter(self):
-        """Return perimeter of Rectangle."""
+        '''Return perimeter of Rectangle.'''
         if self.__width == 0 or self.__height == 0:
             return 0
         return ((self.__width * 2) + (self.__height * 2))
 
     def __Str__(self):
-        """Return printable representation of the Rectangle.
-
-        Print the Rectangle with the character '#'.
-        """
+        '''Return printable representation of the Rectangle.'''
         newrect = []
         if self.__width != 0 and self.__height != 0:
             newrect += "\n".join("#" * self.__width for x in range(self.__height))
@@ -63,9 +60,9 @@ class Rectangle:
         return newrect
 
     def __repr__(self):
-        """Return string represent the Rectangle."""
+        '''Return string represent the Rectangle.'''
         return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
 
     def __del__(self):
-        """Print  the message Bye Rectangle..."""
+        '''Print  the message Bye Rectangle.'''
         print("Bye rectangle...")
