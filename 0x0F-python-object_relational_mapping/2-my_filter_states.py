001e4808ca9177d2881f787a50803d3fb2b7578e
AyaTarek95 <aya.tarek213@icloud.com>
2023-12-15 04:02:48 -0800

    task2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          password=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("select * from states where name like '{}'"
+    cur.execute("select * from states where name like binary '{}'"
                 .format(sys.argv[4]))
     rows = cur.fetchall()
     for row in rows:
