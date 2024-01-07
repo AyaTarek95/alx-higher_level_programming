e21b812d96bdfe6d03db0c59f70aaa597b3e165e
AyaTarek95 <aya.tarek213@icloud.com>
2024-01-07 08:46:32 -0800

    task4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""script that fetch
+https://alx-intranet.hbtn.io/status
+"""
+
+
+import requests
+
+
+if __name__ == "__main__":
+    res = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(res.text)))
+    print("\t- content: {}".format(res.text))
