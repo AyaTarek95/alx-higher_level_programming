f5ede668cd14b0c1ec64c4a438b9662dd7619687
AyaTarek95 <aya.tarek213@icloud.com>
2024-01-07 09:06:01 -0800

    task6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -12,5 +12,5 @@ import requests
 if __name__ == "__main__":
     url = sys.argv[1]
     value = {"email": sys.argv[2]}
-    response = requests.post(url, value)
+    response = requests.post(url, data=value)
     print(response.text)
