0b7a6e87049ca1fbbc660807a1d56cc458eca4f3
AyaTarek95 <aya.tarek213@icloud.com>
2024-01-07 06:06:23 -0800

    fix task0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,9 +7,8 @@ https://alx-intranet.hbtn.io/status
 if __name__ == '__main__':
     import urllib.request
 
-    with urllib.request.urlopen\
-    ('https://alx-intranet.hbtn.io/status') as response:
-        the_page = response.read()
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        the_page = res.read()
         print('Body response:')
         print('\t- type: {}'.format(type(the_page)))
         print('\t- content: {}'.format(the_page))
